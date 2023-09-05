703690a30f54891d8260d1428d66a30fc8e2f014
happycod3r <happy2code0101@gmail.com>
2023-09-05 01:50:19 -0400

    chore: Updated fs_walk

--- fs_walk.py
+++ fs_walk.py
@@ -1,8 +1,30 @@
 import os
-import sys
 from typing import Tuple
 import unicodedata
 import keyboard as kb
+from datetime import datetime as dt
+
+FS_CACHE = []
+SEARCH_CACHE = []
+
+def _cache():
+    with open("cache.txt", "w") as file:
+        for i in range(len(SEARCH_CACHE)):
+            file.write(SEARCH_CACHE[i]) 
+        file.close()
+
+def _check_fs_cache(query: str):
+    for i in range(len(FS_CACHE)):
+        for j in range(len(FS_CACHE[i])):
+            if query in FS_CACHE[i][j]:
+                return FS_CACHE[i]
+    return None
+
+def _check_search_cache(query: str):
+    for i in range(len(SEARCH_CACHE)):
+        if query in SEARCH_CACHE[i]:
+            return SEARCH_CACHE[i]
+    return None
 
 def _is_folder(path: str):
     """ Wrapper function for os.path.isdir() """
@@ -10,11 +32,8 @@ def _is_folder(path: str):
         return True
     return False
 
-FS_CACHE = []
-SEARCH_CACHE = []
-
 def filter_fs(item: Tuple[str, int], file_type: str=None, item_type: str=None): # item_type "dir", "file" or "all"
-    """ Applies the given filter to each step and yields the result. """
+    """ Applies the given filter to each step and yields the relative results. """
     if file_type is None:
         if item_type is None or item_type == "all":
             yield item
@@ -49,29 +68,62 @@ def fswalk(path: str):
 
 def start_fswalk(filter):
     """ Begins the walk through the file system and applies the given filter
-    at each step. Also keeps track of the current directory. Yields the 
-    result and it's current directory. """
+    at each step. Also keeps track of the current directory, caches each step
+    on the walk and yields the relevant results and it's current directory. """
     current_dir = None
     for (i, step) in enumerate(fswalk("C:\\")):
         if step[1] == "dir":
             current_dir = step[0]
         for item in filter_fs(step, filter):
-            yield (item, os.path.dirname(os.path.realpath(current_dir)))
-        
+            result_path = os.path.dirname(os.path.realpath(current_dir))
+            yield (item, result_path)
+ 
+def timer(start, end):
+    _starttime = start.split(':')
+    _starttime = [int(timeseg) for timeseg in _starttime]
+    _endtime = end.split(':')
+    _endtime = [int(timeseg) for timeseg in _endtime]
+    time_taken = ''
+    for i in range(2):
+        diff = _endtime[i] - _starttime[i]
+        time_taken += str(diff) + ":"
+    return time_taken
+
+def init_tui():
 #//////////// TUI //////
-finished = False
-search_filters = []
-while not finished:
-    search_filters.append(input("Search: "))
-    if search_filters[0] == "-q":
-        break
-    if search_filters[-1] == "-q":
-        search_filters.pop(-1)
-        print("SEARCHING FOR %s" % search_filters)
-        for i in range(len(search_filters)):
-            for (count, step) in enumerate(start_fswalk(search_filters[i])):
-                print(count, step[0][0], step[1]) # do something with step here???
-                SEARCH_CACHE.append(step)
-                print(SEARCH_CACHE) # ex. [('Flag987.flag', 'file')]
-                finished = True  
-                break
+    finished = False
+    search_filters = []
+    while not finished:
+        search_filters.append(input("Search: "))
+        if search_filters[0] == "-q":
+            break
+        if search_filters[-1] == "-q":
+            search_filters.pop(-1)
+            print("SEARCHING FOR %s" % search_filters)
+            starttime = dt.now().strftime("%M:%S")
+            for i in range(len(search_filters)):
+                for (count, step) in enumerate(start_fswalk(search_filters[i])):
+                    print(count, step[0][0], step[1])
+                    # Break out of the program if there are no more search
+                    # filters to process.
+                    # Removing finished = True or never assigning it a True value
+                    # will start a new search after finishing the current search_filters 
+                    endtime = dt.now().strftime("%M:%S")
+                    print(starttime, endtime)
+                    finished = True  
+                    break
+
+init_tui()
+
+# Flag988.flag C:\Users\paulm
+# t1 - 14:17 14:42 - 25s
+# t2 - 16:19 16:42 - 23s
+# t3 - 21:36 21:59 - 23s
+# t4 - 24:18 24:41 - 23s
+
+# Flag987.flag C:\Users\paulm
+# t1 - 14:17 15:08 - 51s
+# t2 - 16:19 17:05 - 46s
+# t3 - 21:36 22:22 - 46s
+# t4 - 24:18 25:04 - 46s
+
